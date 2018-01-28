4ff9109783655505dc2fed6daf063698b459d213
Christopher P. Dundon <chris.dundon@gmail.com>
2018-01-28 02:39:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function takeANumber(line, name) {
 }
 
 function currentLine(line) {
-  if (line.length === 0) then {
+  if (line.length === 0) {
     return "The line is currently empty."  
   }
   
